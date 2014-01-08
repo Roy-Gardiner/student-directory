eb6b07c9b45a026350ac2d3ee90c51fbecc4bd4a
Roy Gardiner <roy.gardiner@btopenworld.com>
2014-01-08 13:40:17 +0000

    First Ruby code

---
+++ directory.rb
@@ -0,0 +1,19 @@
+puts "The Students of my cohort at Makers Academy"
+puts "==========================================="
+puts "Berta Gutierrez"
+puts "Megan Folsom"
+puts "Rolando Barbella"
+puts "James Hunter"	
+puts "Abraham Thomas"
+puts "Roy Gardiner"	 
+puts "Jorja Tracy Hung"	
+puts "Colin Frankish"	
+puts "yaz Goulamabasse"	
+puts "Kalle Malmiharju"	
+puts "puts "Andrey Krahmal	
+puts "Biwek Shrestha	
+puts "Mihai-Liviu Cojocar	
+puts "Makis Otman"
+puts "Nabin Rai" 
+puts "Peter Saxton"
+puts "Overall we have"
