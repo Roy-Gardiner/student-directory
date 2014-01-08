f9b03897cb050f398fbf7faa0b60683234240028
Roy Gardiner <roy.gardiner@btopenworld.com>
2014-01-08 14:21:40 +0000

    Array added instead of variables

--- directory.rb
+++ directory.rb
@@ -1,25 +1,30 @@
-student_count = 16
-
 # first, print the list of students
 puts "The Students of my cohort at Makers Academy"
 puts "==========================================="
-puts "Berta Gutierrez"
-puts "Megan Folsom"
-puts "Rolando Barbella"
-puts "James Hunter"	
-puts "Abraham Thomas"
-puts "Roy Gardiner"	 
-puts "Jorja Tracy Hung"	
-puts "Colin Frankish"	
-puts "yaz Goulamabasse"	
-puts "Kalle Malmiharju"	
-puts "Andrey Krahmal"	
-puts "Biwek Shrestha"	
-puts "Mihai-Liviu Cojocar"	
-puts "Makis Otman"
-puts "Nabin Rai" 
-puts "Peter Saxton"
+
+students = [
+ "Berta Gutierrez",
+ "Megan Folsom",
+ "Rolando Barbella",
+ "James Hunter",	
+ "Abraham Thomas",
+ "Roy Gardiner",	 
+ "Jorja Tracy Hung",	
+ "Colin Frankish",	
+ "yaz Goulamabasse",	
+ "Kalle Malmiharju",	
+ "Andrey Krahmal",	
+ "Biwek Shrestha",	
+ "Mihai-Liviu Cojocar",	
+ "Makis Otman",
+ "Nabin Rai", 
+ "Peter Saxton"
+]
+
+students.each do |student|
+   puts student
+end   
 
 # Then print the total number
 
-print "Overall we have #{student_count} fine Students"
+puts "Overall we have #{students.length} fine Students"
